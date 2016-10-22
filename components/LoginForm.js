43085d8c848106182f6794d22c6b9730cf969a2d
Alex Tulenko <ajtulenk>
2016-10-22 15:04:58 -0400

    Tulenko First Push

---
+++ components/LoginForm.js
@@ -0,0 +1,15 @@
+/**
+ * Created by wic on 10/22/2016.
+ */
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+var loginForm = React.createClass({
+   render: function(){
+       return(
+           <div>
+               <h1>Hello, world!</h1>
+           </div>
+       );
+   }
+});
