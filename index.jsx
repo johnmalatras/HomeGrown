18b5b2e6f0c227c7cae18df6b6556af44aee1a3a
John <john.malatras@gmail.com>
2016-10-22 12:46:33 -0400

    Initial commit

---
+++ index.jsx
@@ -0,0 +1,12 @@
+var firebase = require("firebase");
+
+// Initialize Firebase
+// TODO: Replace with your project's customized code snippet
+var config = {
+    apiKey: "AIzaSyCMNnrLwBozPpfG8d4YzCi9W334FhcorEg",
+    authDomain: "homegrown-65645.firebaseapp.com",
+    databaseURL: "https://homegrown-65645.firebaseio.com",
+    storageBucket: "homegrown-65645.appspot.com",
+    messagingSenderId: "818910687408"
+};
+firebase.initializeApp(config);
