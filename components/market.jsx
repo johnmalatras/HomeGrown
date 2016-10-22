26ba87eea0893d7ceb41e50c59745c4acfec9eea
John Malatras <john.malatras@gmail.com>
2016-10-22 17:27:31 -0400

    Added bootstrap
    added bootstrap

--- components/market.jsx
+++ components/market.jsx
@@ -1,9 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+var ReactBootstrap = require('react-bootstrap');
 
 class Market extends React.Component {
     render() {
-        return <button type="button" class="btn btn-default">Left</button>
+    	var ButtonToolbar = ReactBootstrap.ButtonToolbar;
+    	var Button = ReactBootstrap.Button;
+    	const buttonsInstance = (
+		  <ButtonToolbar>
+		    {/* Standard button */}
+		    <Button>Default</Button>
+
+		    {/* Provides extra visual weight and identifies the primary action in a set of buttons */}
+		    <Button bsStyle="primary">Primary</Button>
+
+		    </hr>
+		    
+		    {/* Indicates a successful or positive action */}
+		    <Button bsStyle="success">Success</Button>
+
+		    {/* Contextual button for informational alert messages */}
+		    <Button bsStyle="info">Info</Button>
+
+		    {/* Indicates caution should be taken with this action */}
+		    <Button bsStyle="warning">Warning</Button>
+
+		    {/* Indicates a dangerous or potentially negative action */}
+		    <Button bsStyle="danger">Danger</Button>
+
+		    {/* Deemphasize a button by making it look like a link while maintaining button behavior */}
+		    <Button bsStyle="link">Link</Button>
+		  </ButtonToolbar>
+		);
+        return buttonsInstance;
     }
 }
 
