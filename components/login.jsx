6871dd9b165bab6b43831812a894f6129064a0af
John <john.malatras@gmail.com>
2016-10-22 14:00:01 -0400

    Added webpack, babel & set up hierarchy

--- components/login.jsx
+++ components/login.jsx
@@ -1,3 +1,11 @@
-var ExampleComponent = React.createClass({
-    mixins: [ReactFireMixin]
-});
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+// var ExampleComponent = React.createClass({
+//     mixins: [ReactFireMixin]
+// });
+
+ReactDOM.render(
+    <h1>Hello, world!</h1>,
+    document.getElementById('container')
+);
