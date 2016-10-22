be8e44b0d6e83a7f0ba7277bf12e8ba45f2567c6
John <john.malatras@gmail.com>
2016-10-22 15:04:24 -0400

    added bootstrap

--- components/login.jsx
+++ components/login.jsx
@@ -1,9 +1,6 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
 
-// var ExampleComponent = React.createClass({
-//     mixins: [ReactFireMixin]
-// });
 
 ReactDOM.render(
     <h1>Hello, world!</h1>,
