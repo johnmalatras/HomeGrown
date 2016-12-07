00a45c12dc94909d45ec18ad681a41a74764cf6a
Alex Tulenko <ajtulenko123@gmail.com>
2016-12-06 21:40:33 -0500

    Added Terms of Use

---
+++ webpack.config.production.js
@@ -0,0 +1,39 @@
+/**
+ * Created by alextulenko on 12/6/16.
+ */
+var Webpack = require('webpack');
+var path = require('path');
+var nodeModulesPath = path.resolve(__dirname, 'node_modules');
+var buildPath = path.resolve(__dirname, 'public', 'build');
+var mainPath = path.resolve(__dirname, 'main.js');
+var config = {
+
+    // We change to normal source mapping
+    devtool: 'source-map',
+    entry: mainPath,
+    output: {
+        path: buildPath,
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            },
+            {
+                test: /\.css$/,
+                loader: "style-loader!css-loader"
+            },
+            {
+                test: /\.jpe?g$|\.ico$|\.gif$|\.png$|\.svg$|\.woff$|\.ttf$|\.wav$|\.mp3$/,
+                loader: 'file-loader?name=[name].[ext]'  // <-- retain original file name
+            }
+        ]
+    }
+};
+module.exports = config;
